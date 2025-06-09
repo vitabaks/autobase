414ffa86692f9453e67151c2847b604aac845f7d
Vitaliy Kukharik <vitabaks@gmail.com>
2025-06-09 12:01:17 +0700

    Update the Console UI icons

--- console/ui/src/features/bradcrumbs/ui/index.tsx
+++ console/ui/src/features/bradcrumbs/ui/index.tsx
@@ -3,7 +3,7 @@ import BreadcrumbsItem from '@entities/breadcumb-item';
 import useBreadcrumbs from '@/features/bradcrumbs/hooks/useBreadcrumbs.tsx';
 import { Breadcrumbs as MaterialBreadcrumbs, Icon, Typography } from '@mui/material';
 import RouterPaths from '@app/router/routerPathsConfig';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import HomeOutlinedIcon from '@assets/HomeOutlinedIcon.svg?react';
 import { generateAbsoluteRouterPath } from '@shared/lib/functions.ts';
 import { Link } from 'react-router-dom';
 
