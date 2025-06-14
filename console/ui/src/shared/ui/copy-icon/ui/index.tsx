8cfe916a3c4aaa205247f11e83af47e29bfb8c6c
Vitaliy Kukharik <vitabaks@gmail.com>
2025-06-14 22:05:44 +0700

    Update CopyIcon

--- console/ui/src/shared/ui/copy-icon/ui/index.tsx
+++ console/ui/src/shared/ui/copy-icon/ui/index.tsx
@@ -1,15 +1,15 @@
 import { FC } from 'react';
 import { CopyIconProps } from '@shared/ui/copy-icon/model/types.ts';
 import { useCopyToClipboard } from '@shared/lib/hooks.tsx';
-import CopyValueIcon from '@shared/ui/copy-icon/assets/copyIcon.svg?react';
-import { Box } from '@mui/material';
+import CopyValueIcon from '@mui/icons-material/ContentCopyOutlined';
+import { Box, SxProps, Theme } from '@mui/material';
 
-const CopyIcon: FC<CopyIconProps> = ({ valueToCopy }) => {
+const CopyIcon: FC<CopyIconProps & { sx?: SxProps<Theme> }> = ({ valueToCopy, sx }) => {
   const [_, copyFunction] = useCopyToClipboard();
 
   return (
-    <Box onClick={() => copyFunction(valueToCopy)} sx={{ cursor: 'pointer' }}>
-      <CopyValueIcon />
+    <Box onClick={() => copyFunction(valueToCopy)} sx={{ cursor: 'pointer', ...sx }}>
+      <CopyValueIcon sx={{ fontSize: 'inherit' }} />
     </Box>
   );
 };
