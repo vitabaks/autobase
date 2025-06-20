7d1386728a32fdd5dbb62ce2e93d104be3c5701d
luffy-orf <ghatuletejas@gmail.com>
2025-06-20 08:00:50 +0530

    fix: dropdown

--- console/ui/src/features/clusters-table-row-actions/ui/index.tsx
+++ console/ui/src/features/clusters-table-row-actions/ui/index.tsx
@@ -1,21 +1,42 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { TableRowActionsProps } from '@shared/model/types.ts';
-import ClustersTableRemoveButton from '@features/clusters-table-row-actions/ui/ClusterTableRemoveButton.tsx';
-import ClustersTableExportButton from '@features/clusters-table-row-actions/ui/ClusterTableExportButton.tsx';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import ClustersTableRemoveButton from './ClusterTableRemoveButton';
+import ClustersTableExportButton from './ClusterTableExportButton';
 
-const ClustersTableRowActions: FC<TableRowActionsProps> = ({ closeMenu, row }) => [
-  <ClustersTableExportButton
-    key={0}
-    clusterId={row.original.id}
-    clusterName={row.original.name.props.children}
-    closeMenu={closeMenu}
-  />,
-  <ClustersTableRemoveButton
-    key={1}
-    clusterId={row.original.id}
-    clusterName={row.original.name.props.children}
-    closeMenu={closeMenu}
-  />,
-];
+const ClustersTableRowActions: FC<TableRowActionsProps> = ({ closeMenu, row }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget);
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    closeMenu();
+  };
+
+  return (
+    <>
+      <IconButton onClick={handleMenuOpen} size="small">
+        <MoreVertIcon />
+      </IconButton>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+        <MenuItem disableGutters>
+          <ClustersTableExportButton
+            clusterId={row.original.id}
+            clusterName={row.original.name.props.children}
+            closeMenu={handleMenuClose}
+          />
+        </MenuItem>
+        <MenuItem disableGutters>
+          <ClustersTableRemoveButton
+            clusterId={row.original.id}
+            clusterName={row.original.name.props.children}
+            closeMenu={handleMenuClose}
+          />
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
 
 export default ClustersTableRowActions;
